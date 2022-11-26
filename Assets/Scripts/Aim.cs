0d26071dea360092fce55cd842bb90a1c4d70261
adamski201 <lanster993@hotmail.com>
2022-11-26 17:42:50 +0000

    renamed script

--- Assets/Scripts/Aim.cs
+++ Assets/Scripts/Aim.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AimWeapon : MonoBehaviour
+public class Aim : MonoBehaviour
 {
     private Transform aimTransform;
 
