e52d2171c985e6068403ea07e03733f115821152
Adam O'Sullivan <90043609+adamski201@users.noreply.github.com>
2023-06-12 12:25:44 +0100

    improved recoil

--- Assets/Scripts/CursorController.cs
+++ Assets/Scripts/CursorController.cs
@@ -6,12 +6,17 @@ public class CursorController : MonoBehaviour
 {
     [SerializeField] private Texture2D[] crosshairTextureArray;
     [SerializeField] private int frameCount;
-    [SerializeField] private float frameRate;
+    [SerializeField] private float recoilTime;
     private int currentFrame;
+    private bool recoil = false;
     private float frameTimer;
+    private float animChangeTimer;
 
     void Awake()
     {
+
+        Application.targetFrameRate = 144;
+
         //set the cursor origin to its centre. (default is upper left corner)
         Vector2 cursorOffset = new Vector2(crosshairTextureArray[0].width / 2, crosshairTextureArray[0].height / 2);
 
@@ -21,14 +26,44 @@ public class CursorController : MonoBehaviour
 
     private void Update()
     {
-        frameTimer -= Time.deltaTime;
-        if (frameTimer <= 0f)
+        if (recoil)
         {
-            frameTimer += frameRate;
-            currentFrame = (currentFrame + 1) % frameCount;
-            Cursor.SetCursor(crosshairTextureArray[currentFrame]
-                            , new Vector2(crosshairTextureArray[currentFrame].width / 2
-                            , crosshairTextureArray[currentFrame].height), CursorMode.ForceSoftware);
+            frameTimer += Time.deltaTime;
+            if (frameTimer >= animChangeTimer)
+            {
+                animChangeTimer += (recoilTime / frameCount);
+                currentFrame += 1;
+                Cursor.SetCursor(crosshairTextureArray[currentFrame]
+                                , new Vector2(crosshairTextureArray[currentFrame].width / 2
+                                , crosshairTextureArray[currentFrame].height / 2), CursorMode.ForceSoftware);
+            }
+
+            if (frameTimer >= 0)
+            {
+                StopRecoilAnimation();
+            }
         }
     }
+
+    public void StartRecoilAnimation()
+    {
+        frameTimer -= recoilTime;
+        animChangeTimer += frameTimer + (recoilTime / frameCount);
+        recoil = true;
+    }
+
+    private void StopRecoilAnimation()
+    {
+        frameTimer = 0;
+        animChangeTimer = 0;
+        currentFrame = 0;
+        recoil = false;
+
+        Vector2 cursorOffset = new Vector2(crosshairTextureArray[0].width / 2, crosshairTextureArray[0].height / 2);
+        Cursor.SetCursor(crosshairTextureArray[0], cursorOffset, CursorMode.ForceSoftware);
+    }
 }
+
+/*Cursor.SetCursor(crosshairTextureArray[currentFrame]
+                                , new Vector2(crosshairTextureArray[currentFrame].width / 2
+                                , crosshairTextureArray[currentFrame].height / 2), CursorMode.ForceSoftware);*/
--- Assets/Scripts/ShootPistol.cs
+++ Assets/Scripts/ShootPistol.cs
@@ -1,21 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ShootPistol : MonoBehaviour
 {
     [SerializeField] private Transform firePoint;
-    [SerializeField] private float range = 10f, recoilTime = 1.5f;
+    [SerializeField] private float range = 10f, recoilTime = 0.3f;
     [SerializeField] private int damage = 20;
     [SerializeField] private int knockbackPower = 2;
     private bool recoil = false;
+    [SerializeField] private UnityEvent shotFired;
+    private float recoilTimer;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        // Shoot upon mouse button click.
+        if (!recoil)
         {
-            Shoot();
+            if (Input.GetMouseButtonDown(0))
+            {
+                Shoot();
+            }
+        }
+
+        // Check if recoil period is over.
+        if (recoil && Time.time >= recoilTimer)
+        {
+            recoil = false;
         }
     }
 
@@ -24,17 +37,16 @@ public class ShootPistol : MonoBehaviour
         // Handles shooting logic using raycasts
         RaycastHit2D hit = Physics2D.Raycast(firePoint.position, firePoint.right, range);
 
-        if (!recoil)
+        if (hit)
         {
-            if (hit)
-            {
-                GameObject hitObject = hit.transform.gameObject;
-                TryHitObject(hitObject);
-            }
-
-            recoil = true;
-            Invoke("resetRecoil", recoilTime);
+            GameObject hitObject = hit.transform.gameObject;
+            TryHitObject(hitObject);
         }
+
+        recoilTimer = Time.time + recoilTime;
+        recoil = true;
+
+        shotFired.Invoke();
     }
 
     void TryHitObject(GameObject objectToHit)
@@ -43,10 +55,5 @@ public class ShootPistol : MonoBehaviour
         {
             hittableObject.TakeHit(damage, knockbackPower, gameObject.transform.position);
         }
-    }
-
-    private void resetRecoil()
-    {
-        recoil = false;
     }
 }
